34e8432184b0176b684036b69e968edd38a4de48
jumperM4 <pma1994ars@yandex.ru>
2023-06-15 20:01:00 +0300

    app with functional method is ready

--- src/components/searchBar/searchBar.component.jsx
+++ src/components/searchBar/searchBar.component.jsx
@@ -1,17 +1,14 @@
-import { Component } from "react";
 import "./searchBar.styles.css";
 
-class SearchBar extends Component {
-  render() {
-    return (
-      <input
-        className={`search-box ${this.props.className}`}
-        type="search"
-        placeholder={this.props.placeholder}
-        onChange={this.props.onChangeHandler}
-      />
-    );
-  }
-}
+const SearchBar = ({ className, placeholder, onChangeHandler }) => {
+  return (
+    <input
+      className={`search-box ${className}`}
+      type="search"
+      placeholder={placeholder}
+      onChange={onChangeHandler}
+    />
+  );
+};
 
 export default SearchBar;
